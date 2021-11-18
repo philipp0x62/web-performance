914a1bd32e0dff37d4942a8d8f1e74c0245b2fa5
Luca Schumann <schumann_luca@yahoo.de>
2021-11-18 15:56:18 +0100

    first commit

---
+++ run_measurements.py
@@ -0,0 +1,282 @@
+import re
+import selenium.common.exceptions
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as firefoxOptions
+from selenium.webdriver.chrome.options import Options as chromeOptions
+import sys
+from tranco import Tranco
+import sqlite3
+from datetime import datetime
+import hashlib
+import uuid
+import os
+
+# download top list
+t = Tranco(cache=True, cache_dir='.tranco')
+tranco_list = t.list(date='2021-10-18')
+pages = tranco_list.top(25)
+
+# performance elements to extract
+measurement_elements = ('id', 'protocol', 'server', 'domain', 'timestamp', 'connectEnd', 'connectStart', 'domComplete',
+                        'domContentLoadedEventEnd', 'domContentLoadedEventStart', 'domInteractive', 'domainLookupEnd',
+                        'domainLookupStart', 'duration', 'encodedBodySize', 'decodedBodySize', 'transferSize',
+                        'fetchStart', 'loadEventEnd', 'loadEventStart', 'redirectCount', 'redirectEnd', 'redirectStart',
+                        'requestStart', 'responseEnd', 'responseStart', 'secureConnectionStart', 'startTime',
+                        'unloadEventEnd', 'unloadEventStart', 'workerStart', 'nextHopProtocol', 'cacheWarming', 'error')
+
+# create db
+db = sqlite3.connect('web-performance.db')
+cursor = db.cursor()
+
+# retrieve input params
+try:
+    protocol = sys.argv[1]
+    server = sys.argv[2]
+    proxyPID = int(sys.argv[3])
+except IndexError:
+    print("Input params incomplete (protocol, server, dnsproxyPID) - set dnsproxyPID to 0 if you dont use dnsproxy")
+    sys.exit(1)
+
+if len(sys.argv) > 4:
+    browser = sys.argv[4]
+else:
+    browser = 'firefox'
+
+# Chrome options
+chrome_options = chromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+# Firefox options
+firefox_options = firefoxOptions()
+firefox_options.add_argument('--headless')
+
+
+def create_driver():
+    if browser == 'chrome':
+        return webdriver.Chrome(options=chrome_options)
+    else:
+        return webdriver.Firefox(options=firefox_options)
+
+
+def get_page_performance_metrics(driver, page):
+    script = """
+            // Get a resource performance entry
+            var perfEntries = performance.getEntriesByType("navigation");
+
+            var entry = perfEntries[0];
+
+            // Get the JSON
+            return entry.toJSON();
+            """
+    try:
+        driver.get(f'https://{page}')
+        return driver.execute_script(script)
+    except selenium.common.exceptions.WebDriverException as e:
+        return {'error': str(e)}
+
+
+def perform_page_load(page, cache_warming=0):
+    driver = create_driver()
+    timestamp = datetime.now()
+    performance_metrics = get_page_performance_metrics(driver, page)
+    driver.quit()
+    # insert page into database
+    if 'error' not in performance_metrics:
+        insert_performance(page, performance_metrics, timestamp, cache_warming=cache_warming)
+    else:
+        insert_performance(page, {k: 0 for k in measurement_elements}, timestamp, cache_warming=cache_warming,
+                           error=performance_metrics['error'])
+    # send restart signal to dnsProxy after loading the page
+    if proxyPID != 0:
+        os.system("sudo kill -SIGUSR1 %d" % proxyPID)
+
+
+def create_measurements_table():
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS measurements (
+            id string,
+            protocol string,
+            server string,
+            domain string,
+            timestamp datetime,
+            connectEnd integer,
+            connectStart integer,
+            domComplete integer,
+            domContentLoadedEventEnd integer,
+            domContentLoadedEventStart integer,
+            domInteractive integer,
+            domainLookupEnd integer,
+            domainLookupStart integer,
+            duration integer,
+            encodedBodySize integer,
+            decodedBodySize integer,
+            transferSize integer,
+            fetchStart integer,
+            loadEventEnd integer,
+            loadEventStart integer,
+            redirectCount integer,
+            redirectEnd integer,
+            redirectStart integer,
+            requestStart integer,
+            responseEnd integer,
+            responseStart integer,
+            secureConnectionStart integer,
+            startTime integer,
+            unloadEventEnd integer,
+            unloadEventStart integer,
+            workerStart integer,
+            nextHopProtocol string,
+            cacheWarming integer,
+            error string,
+            PRIMARY KEY (id)
+        );
+        """)
+    db.commit()
+
+
+def create_lookups_table():
+    cursor.execute("""
+            CREATE TABLE IF NOT EXISTS lookups (
+                measurement_id string,
+                domain string,
+                elapsed numeric,
+                status string,
+                answer string,
+                FOREIGN KEY (measurement_id) REFERENCES measurements(id)
+            );
+            """)
+    db.commit()
+
+
+def create_qlogs_table():
+    cursor.execute("""
+            CREATE TABLE IF NOT EXISTS qlogs (
+                measurement_id string,
+                qlog string,
+                FOREIGN KEY (measurement_id) REFERENCES measurements(id)
+            );
+            """)
+    db.commit()
+
+
+def insert_performance(page, performance, timestamp, cache_warming=0, error=''):
+    performance['protocol'] = protocol
+    performance['server'] = server
+    performance['domain'] = page
+    performance['timestamp'] = timestamp
+    performance['cacheWarming'] = cache_warming
+    performance['error'] = error
+    # generate unique ID
+    sha = hashlib.md5()
+    sha_input = ('' + protocol + server + page + str(cache_warming))
+    sha.update(sha_input.encode())
+    uid = uuid.UUID(sha.hexdigest())
+    performance['id'] = str(uid)
+
+    # insert into database
+    cursor.execute(f"""
+    INSERT INTO measurements VALUES ({(len(measurement_elements) - 1) * '?,'}?);
+    """, tuple([performance[m_e] for m_e in measurement_elements]))
+    db.commit()
+
+    if protocol == 'quic':
+        insert_qlogs(str(uid))
+    # insert all domain lookups into second table
+    if error == '':
+        insert_lookups(str(uid))
+
+
+def insert_qlogs(uid):
+    with open("qlogs.txt", "r") as qlogs:
+        log = qlogs.read()
+        cursor.execute("""
+            INSERT INTO qlogs VALUES (?,?);
+            """, (uid, log))
+        db.commit()
+    # remove the qlogs after dumping it into the db
+    with open("qlogs.txt", "w") as qlogs:
+        qlogs.write('')
+
+
+def insert_lookup(uid, domain, elapsed, status, answer):
+    cursor.execute("""
+    INSERT INTO lookups VALUES (?,?,?,?,?);
+    """, (uid, domain, elapsed, status, answer))
+    db.commit()
+
+
+def insert_lookups(uid):
+    with open("dnsproxy.log", "r") as logs:
+        lines = logs.readlines()
+        currently_parsing = ''
+        domain = ''
+        elapsed = 0.0
+        status = ''
+        answer = ''
+
+        for line in lines:
+            # upon success
+            if 'successfully finished exchange' in line:
+                if 'tranco-list.eu.' not in line:
+                    currently_parsing = 'success'
+                    domain = re.search('exchange of ;(.*)IN', line).group(1).rstrip()
+                    elapsed = re.search('Elapsed (.*)ms', line)
+                    factor = 1.0
+                    if elapsed is None:
+                        elapsed = re.search('Elapsed (.*)µs', line)
+                        factor = 1.0 / 1000.0
+                    if elapsed is None:
+                        elapsed = re.search('Elapsed (.*)s', line)
+                        factor = 1000.0
+                    elapsed = float(elapsed.group(1)) * factor
+            # upon failure
+            elif 'failed to exchange' in line:
+                currently_parsing = 'failure'
+                domain = re.search('failed to exchange ;(.*)IN', line).group(1).rstrip()
+                answer = re.search('Cause: (.*)', line).group(1).rstrip()
+                elapsed = re.search('in (.*)ms\\.', line)
+                factor = 1.0
+                if elapsed is None:
+                    elapsed = re.search('in (.*)µs\\.', line)
+                    factor = 1.0 / 1000.0
+                if elapsed is None:
+                    elapsed = re.search('in (.*)s\\.', line)
+                    factor = 1000.0
+                elapsed = float(elapsed.group(1)) * factor
+            elif currently_parsing == '':
+                pass
+            elif ', status: ' in line:
+                status = re.search(', status: (.*),', line).group(1)
+                # if failure the parsing stops here, else we wait for the answer section
+                if currently_parsing == 'failure':
+                    insert_lookup(uid, domain, elapsed, status, answer)
+                    currently_parsing = ''
+            elif ';; ANSWER SECTION:' in line:
+                currently_parsing = 'answer'
+                answer = ''
+            elif currently_parsing == 'answer':
+                # in this case we finished parsing the answer section
+                if line.rstrip() == '':
+                    insert_lookup(uid, domain, elapsed, status, answer)
+                    currently_parsing = ''
+                else:
+                    answer += ','.join(line.split())
+                    answer += '|'
+    # remove the log after parsing it
+    with open("dnsproxy.log", "w") as logs:
+        logs.write('')
+
+
+create_measurements_table()
+create_lookups_table()
+create_qlogs_table()
+for p in pages:
+    # cache warming
+    print(f'{p}: cache warming')
+    perform_page_load(p, 1)
+    # performance measurement
+    print(f'{p}: measuring')
+    perform_page_load(p)
+
+db.close()
---
+++ run_measurements.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "starting measurement process..."
+
+# increase UDP receive buffer size
+sudo sysctl -w net.core.rmem_max=25000000
+# stop systemd-resolved and edit resolv.conf
+sudo systemctl stop systemd-resolved
+sudo systemctl disable systemd-resolved
+echo "nameserver 127.0.0.2" | sudo tee /etc/resolv.conf
+
+cd /home/ubuntu/dnsproxy
+
+upstream="94.140.15.15"
+https_upstream="94.140.15.15/dns-query"
+declare -a protocols=("quic" "tls" "https" "tcp" "udp")
+
+for p in "${protocols[@]}"
+do
+	echo $p
+
+	if [ $p = "udp" ]
+	then
+		resolver="${upstream}"
+	elif [ $p = "https" ]
+	then
+		resolver="${p}://${https_upstream}"
+	else
+		resolver="${p}://${upstream}"
+	fi
+
+	sleep 1
+	echo "starting dnsproxy"
+	./dnsproxy -u ${resolver} -v -l "127.0.0.2" >& /home/ubuntu/web-performance/dnsproxy.log &
+	dnsproxyPID=$!
+
+	# measurements
+	sleep 1
+	echo "starting measurements"
+	cd /home/ubuntu/web-performance
+	python3 run_measurements.py $p $upstream $dnsproxyPID chrome
+	cd /home/ubuntu/dnsproxy
+
+	sleep 1
+	echo "killing dnsproxy"
+	sudo kill -SIGTERM $dnsproxyPID
+done
+
+# restart systemd-resolved
+sudo systemctl enable systemd-resolved
+sudo systemctl start systemd-resolved
+
+echo "FIN"
