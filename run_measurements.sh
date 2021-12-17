ad6b21fa71bad65af72570413c716f62ce4d931a
Luca Schumann <schumann_luca@yahoo.de>
2021-12-17 15:08:00 +0100

    added insecure flag

--- run_measurements.sh
+++ run_measurements.sh
@@ -53,7 +53,7 @@ while read upstream; do
 
 		sleep 1
 		echo "starting dnsproxy"
-		./dnsproxy -u ${resolver} -v --ipv6-disabled -l "127.0.0.2" >& /home/ubuntu/web-performance/dnsproxy.log &
+		./dnsproxy -u ${resolver} -v --insecure --ipv6-disabled -l "127.0.0.2" >& /home/ubuntu/web-performance/dnsproxy.log &
 		dnsproxyPID=$!
 
 		# measurements
