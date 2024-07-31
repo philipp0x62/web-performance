4946c4e3c4daf81e29328bb3f1dd447a0a891ae0
philipp0x62 <48681439+philipp0x62@users.noreply.github.com>
2024-07-31 12:32:18 +0200

    adding automatism to download always the latest list

--- preapare_database.py
+++ preapare_database.py
@@ -1,17 +1,30 @@
 import sqlite3
 import requests
 import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+#import time
 
 # just manually get pages list so the domain does not need to be resolved each time
 #pages = ['www.google.com', 'www.netflix.com', 'www.youtube.com', 'www.facebook.com', 'www.microsoft.com', 'twitter.com', 'www.instagram.com', 'www.baidu.com', 'www.linkedin.com', 'www.apple.com', 'www.wikipedia.org', 'www.amazon.com']
 
 
 # connect to db
-db = sqlite3.connect('web-performance.db')
-cursor = db.cursor()
+#db = sqlite3.connect('web-performance.db')
+#cursor = db.cursor()
 
 # get tranco list file
-content = requests.get('https://tranco-list.eu/download/J939Y/full').content.decode('utf-8')
+driver = webdriver.Chrome()
+
+driver.get("https://tranco-list.eu/latest_list")
+element = driver.find_element(By.ID, "btnGroupDrop1")
+element.click()
+download_element = driver.find_element(By.LINK_TEXT, "Download full list")
+#print(download_element.get_attribute('href'))
+
+
+content = requests.get(download_element.get_attribute('href')).content.decode('utf-8')
+#content = requests.get('https://tranco-list.eu/download/J939Y/full').content.decode('utf-8')
 data = list(csv.reader(content.splitlines(), delimiter=','))
 
 #print(data[0])
@@ -32,9 +45,9 @@ id, dns = res.fetchone()
 
 print(f'ID: {id}, DNS Name {dns}')
 
-id, dns = res.fetchone()
+#id, dns = res.fetchone()
 
-print(f'ID: {id}, DNS Name {dns}')
+#print(f'ID: {id}, DNS Name {dns}')
 
 # close connection
 db.close()
